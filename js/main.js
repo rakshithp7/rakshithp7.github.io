5ada9b9ff8fa4463ad4343b13e96cf5af8302396
Rakshith Poojary <rakshithp7@gmail.com>
2020-09-08 20:05:52 +0530

    Add insta clone images and fix up the website styling

--- js/main.js
+++ js/main.js
@@ -191,40 +191,45 @@
 	------------------------------------------------------ */
 
   /* local validation */
-  $("#contactForm").validate({
-    /* submit via ajax */
-    submitHandler: function (form) {
-      var sLoader = $("#submit-loader");
+  // $("#contactForm").validate({
+  //   /* submit via ajax */
+  //   submitHandler: function (form) {
+  //     var sLoader = $("#submit-loader");
+  //     $.ajax({
+  //       type: "POST",
+  //       url: "inc/sendEmail.php",
+  //       data: $(form).serialize(),
+  //       beforeSend: function () {
+  //         sLoader.fadeIn();
+  //       },
+  //       success: function (msg) {
+  //         console.log(msg);
+  //         // Message was sent
+  //         if (msg == "OK") {
+  //           sLoader.fadeOut();
+  //           $("#message-warning").hide();
+  //           $("#contactForm").fadeOut();
+  //           $("#message-success").fadeIn();
+  //         }
+  //         // There was an error
+  //         else {
+  //           sLoader.fadeOut();
+  //           $("#message-warning").html(msg);
+  //           $("#message-warning").fadeIn();
+  //         }
+  //       },
+  //       error: function () {
+  //         sLoader.fadeOut();
+  //         $("#message-warning").html("Something went wrong. Please try again.");
+  //         $("#message-warning").fadeIn();
+  //       },
+  //     });
+  //   },
+  // });
 
-      $.ajax({
-        type: "POST",
-        url: "inc/sendEmail.php",
-        data: $(form).serialize(),
-        beforeSend: function () {
-          sLoader.fadeIn();
-        },
-        success: function (msg) {
-          // Message was sent
-          if (msg == "OK") {
-            sLoader.fadeOut();
-            $("#message-warning").hide();
-            $("#contactForm").fadeOut();
-            $("#message-success").fadeIn();
-          }
-          // There was an error
-          else {
-            sLoader.fadeOut();
-            $("#message-warning").html(msg);
-            $("#message-warning").fadeIn();
-          }
-        },
-        error: function () {
-          sLoader.fadeOut();
-          $("#message-warning").html("Something went wrong. Please try again.");
-          $("#message-warning").fadeIn();
-        },
-      });
-    },
+  $(".sendEmail").on("click", function () {
+    window.location.href =
+      "mailto:rakshithp7@gmail.com?subject=Mail%20from%20github%20portfolio&body=<<message%20goes%20here>>";
   });
 
   /*----------------------------------------------------- */
